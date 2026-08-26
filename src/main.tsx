f70e6a58f6336548097300ece252730638d62a3c
arm25382019-lgtm <arm25382019@gmail.com>
2026-08-26 14:24:55 +0700

    feat: route results page to Travelpayouts live data

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
+import LiveResultsPage from './components/LiveResultsPage';
 import './styles.css';
 import './flight-detail.css';
 import './fare.css';
+import './tripi.css';
+import './live-results.css';
+
+function Root() {
+  const location = useLocation();
+  return location.pathname === '/results' ? <LiveResultsPage /> : <App />;
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 );
