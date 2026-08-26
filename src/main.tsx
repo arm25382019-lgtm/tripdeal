4980c8fa3246dea73f553cebd14bca7a135ae3f9
arm25382019-lgtm <arm25382019@gmail.com>
2026-08-26 14:40:56 +0700

    feat: route live deals through internal TripDeal detail page

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
 import LiveResultsPage from './components/LiveResultsPage';
+import LiveDealPage from './components/LiveDealPage';
 import LegacyFlightBridge from './components/LegacyFlightBridge';
 import './styles.css';
 import './flight-detail.css';
 import './fare.css';
 import './tripi.css';
 import './live-results.css';
+import './live-deal.css';
 
 function Root() {
   const location = useLocation();
   if (location.pathname === '/results') return <LiveResultsPage />;
+  if (location.pathname === '/deal') return <LiveDealPage />;
   if (location.pathname === '/flight') return <LegacyFlightBridge />;
   return <App />;
 }
